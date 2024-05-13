e1ab2da081f78a67177ece44d039a6a56a208af1
Rashmika003 <s19003@sci.pdn.ac.lk>
2024-05-13 17:41:53 +0530

    tailwind css test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
 
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
+
+
 export default function Home() {
   return (
-    <p>hello world</p>
+    <>
+      
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          
+          {/*  {' '} part is used to preserve the white space */}
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Your Marketplace for high-quality{' '}
+            <span className="text-blue-600">digital assets</span>.
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">Welcome to DigitalHippo. Every assest on our platform is verfied by our platform is verified by our team to ensure our highest quality standers
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+
+            {/* this is set shadcn ui library button default  */}
+            <Link href="/products" className={buttonVariants()}>Browse Trending</Link>
+
+            {/* &rarr; <= this is a inbuild right arrow */}
+            {/* we can choose button types from the ui library */}
+            <Button variant="ghost">Our quality promise &rarr;</Button>
+          </div>
+        </div>
+
+        {/* TODO : List products */}
+      
+      </>
   );
 }
 
