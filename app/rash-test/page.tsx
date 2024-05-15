944b45d87f3c9ce8d644b21b723cf91ca48b25b5
Rashmika003 <s19003@sci.pdn.ac.lk>
2024-05-15 18:00:11 +0530

    tailwind test components was added

---
+++ app/rash-test/page.tsx
@@ -0,0 +1,11 @@
+const Test = () => {
+    return (
+        <>
+            <h1 className="text-red-900" >hi there, this is test for tailwind css</h1>
+
+        
+        </>
+    )
+}
+
+export default Test
