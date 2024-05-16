e47eb86b2f2c3ac23c617e931b150eec7560ea02
sachith suvimal <137740959+sachith-953@users.noreply.github.com>
2024-05-16 16:07:05 +0530

    signup page untill add svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,45 @@
 
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Button, buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
+// import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+// import { Button, buttonVariants } from "@/components/ui/button";
+// import Link from "next/link";
 
 
+
+// export default function Home() {
+//   return (
+//     <>
+//       <MaxWidthWrapper>
+//         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          
+//           {/*  {' '} part is used to preserve the white space */}
+//           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Your Marketplace for high-quality{' '}
+//             <span className="text-blue-600">Sri Lanka Products</span>.
+//           </h1>
+//           <p className="mt-6 text-lg max-w-prose text-muted-foreground">Welcome to Heart Of Ceylon. Every product on our platform is verfied by our team to ensure our highest quality standers
+//           </p>
+//           <div className="flex flex-col sm:flex-row gap-4 mt-6">
+
+//             {/* this is set shadcn ui library button default  */}
+//             <Link href="/products" className={buttonVariants()}>Browse Trending</Link>
+
+//             {/* &rarr; <= this is a inbuild right arrow */}
+//             {/* we can choose button types from the ui library */}
+//             <Button variant="ghost">Our quality promise &rarr;</Button>
+//           </div>
+//         </div>
+
+//         {/* TODO : List products */}
+//       </MaxWidthWrapper>
+//       </>
+//   );
+// }
+
+import Signup from "@/components/signup/page";
 
 export default function Home() {
   return (
-    <>
-      <MaxWidthWrapper>
-        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-          
-          {/*  {' '} part is used to preserve the white space */}
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Your Marketplace for high-quality{' '}
-            <span className="text-blue-600">Sri Lanka Products</span>.
-          </h1>
-          <p className="mt-6 text-lg max-w-prose text-muted-foreground">Welcome to Heart Of Ceylon. Every product on our platform is verfied by our team to ensure our highest quality standers
-          </p>
-          <div className="flex flex-col sm:flex-row gap-4 mt-6">
-
-            {/* this is set shadcn ui library button default  */}
-            <Link href="/products" className={buttonVariants()}>Browse Trending</Link>
-
-            {/* &rarr; <= this is a inbuild right arrow */}
-            {/* we can choose button types from the ui library */}
-            <Button variant="ghost">Our quality promise &rarr;</Button>
-          </div>
-        </div>
-
-        {/* TODO : List products */}
-      </MaxWidthWrapper>
-      </>
+    <div>
+      <Signup />
+    </div>
   );
 }
-
