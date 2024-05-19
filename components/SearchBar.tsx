a4e1eb6349f6dc7de58102ee790681c8543c3011
JanathBandara <janathBandara@gmail.com>
2024-05-19 14:54:21 +0530

    Create SearchBar.tsx

---
+++ components/SearchBar.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+function onSearch() {}
+
+export default function SearchBar() {
+  return (
+    <MaxWidthWrapper>
+      <div>
+        <div className="flex flex-col gap-10 items-center p-6">
+          <form className="flex justify-center w-2/3" onSubmit={onSearch}>
+            <input
+              className="px-5 py-1 w-2/3 sm:px-5 sm:py-3 flex-1 text-zinc-600 bg-slate-300 focus:big-black rounded-full focus:outline-none"
+              placeholder="What are you looking for? "
+            ></input>
+          </form>
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  );
+}
