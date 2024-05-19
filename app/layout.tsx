063a31677a8eb006ac65f4c2111274f77296ec2e
MadhushanPradeep <madhushangg99@gmail.com>
2024-05-19 12:15:58 +0530

    commit to madhushan's brach - footer created as a componenet
    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <main className="relative flex flex-col min-h-screen">
           <Navbar />
           <div className="flex-grow flex-1">{children}</div>
+          <Footer/>
         </main>
       </body>
     </html>
