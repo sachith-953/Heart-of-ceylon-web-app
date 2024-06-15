1ea0cb5eb5370d6967b6fd2ec2a2727aa6925f8e
Rashmika003 <s19003@sci.pdn.ac.lk>
2024-06-15 10:02:43 +0530

    keyword suggestion API calls done.

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,19 +1,63 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 
 
 export default function SearchBar() {
-  const router = useRouter();
 
+  const router = useRouter();
   const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  // API Calling function
+  const fetchSuggestions = async (query: string) => {
+    try {
+      console.log("sending keyword to Next.js GetKeyword API");
+
+      const res = await fetch(
+        "http://localhost:3000/api/product/getKeywords",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ query }),
+        }
+      );
+
+      if (res.ok) {
+        const responseData = await res.json();
+
+        console.log(responseData);
+
+        console.log("responseData.productList");
+
+        setSuggestions(responseData);
+
+      } else {
+        const responseData = await res.json();
+        console.log("********failed********")
+      }
+
+    } catch (error) {
+      console.error("Error fetching suggestions:", error);
+    }
+  };
 
   const handleSearch = () => {
     // Update the URL with the search query as a search parameter
     router.push(`search-products?query=${encodeURIComponent(searchQuery)}`);
   };
+
+  useEffect(() => {
+    if (searchQuery.trim() !== "") {
+      fetchSuggestions(searchQuery);
+    } else {
+      setSuggestions([]);
+    }
+  }, [searchQuery]);
 
   return (
     <>
