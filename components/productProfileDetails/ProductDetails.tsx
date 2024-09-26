0be9e3adfa63abb2e7116eaa136c0e2bceef5738
sachith suvimal <137740959+sachith-953@users.noreply.github.com>
2024-09-26 18:27:07 +0530

    Update ProductDetails.tsx

--- components/productProfileDetails/ProductDetails.tsx
+++ components/productProfileDetails/ProductDetails.tsx
@@ -36,7 +36,7 @@ const ProductProfile: React.FC<ProductProfileProps> = ({ product }) => {
             {/* Product details */}
             <div className="w-full md:w-1/3 p-6 flex flex-col justify-between">
                 <div>
-                    <h2 className="text-2xl font-bold text-gray-800 mb-2">Ceylon tea {product?.title}</h2>
+                    <h2 className="text-2xl font-bold text-gray-800 mb-2">Ceylon Tea {product?.title}</h2>
                     <p className="text-gray-600 text-sm mb-4">The finest cup of tea,the name Ceylon had become synonymous with the world's finest tea. {product?.description}</p>
                     <p className="text-xl font-semibold text-green-600 mb-4">
                         LKR {product?.price} / ${((product?.price || 3499.00)).toFixed(2)}
