701a72e1ff19e390b7deb5d2b9045d7ffab0e8ad
Rashmika003 <www.rashmikaabeyrathna2019@gmail.com>
2024-10-22 12:05:39 +0530

    update seller personal details done

--- components/dashboard/sellerDashboard/ManageAccount/ChangeSellerAccountDetails.tsx
+++ components/dashboard/sellerDashboard/ManageAccount/ChangeSellerAccountDetails.tsx
@@ -32,6 +32,8 @@ const ChangeSellerAccountDetails = () => {
     const [sellerAddress, setSellerAddress] = useState("");
     const [district, setDistrict] = useState("");
 
+    const [isReload, setIsReload] = useState(false); // use to reload the component when details updated
+
     const fetchSellerDetails = async () => {
         try {
             const res = await fetch(
@@ -44,8 +46,9 @@ const ChangeSellerAccountDetails = () => {
                 console.log(ResponseData)
                 console.log("successfully changed acc info")
                 setSellerDetails(ResponseData)
+                setIsReload(true)
             }
-            else if (res.status === 403) {    
+            else if (res.status === 403) {
                 // this trigger when referesh token has issure. 
                 // if token is expired this will trigger
                 toast({
@@ -55,7 +58,7 @@ const ChangeSellerAccountDetails = () => {
                 })
                 console.log("****403****************")
                 console.log("Redirectiong to login. RT error")
-                router.push("/seller-log-in"); 
+                router.push("/seller-log-in");
             }
             else {
                 // show error notification in red color
@@ -77,13 +80,66 @@ const ChangeSellerAccountDetails = () => {
         }
     }
 
+    const handleDetailsUpdateForm = async (formData: FormData) => {
+
+        //buttonIsClicked function update useState to track whether 
+        //form is submitted or not. action method cannot use for such things
+
+        try {
+            const res = await fetch(`${BASE_URL}/api/seller-dashboard/update-seller-private-details`, {
+                method: 'POST',
+                body: formData
+            })
+
+            if (res.ok) {
+                const ResponseData = await res.json()
+                console.log(ResponseData)
+                console.log("successfully changed acc info")
+                toast({
+                    title: "Success!",
+                    description: "Successfully changed Your Account Infomation",
+                })
+                setIsReload(!isReload)
+            }
+            else if (res.status === 403) {
+                toast({
+                    variant: "destructive",
+                    title: "Sorry!",
+                    description: "Please Login again. Your Session has Expired!",
+                })
+                console.log("****403****************")
+                console.log("Redirectiong to login. RT error")
+                router.push("/seller-log-in");
+            }
+            else {
+                const ResponseData = await res.json()
+                console.log(ResponseData)
+                // show error notification in red color
+                toast({
+                    variant: "destructive",
+                    title: "Something went wrong.",
+                    description: "Plase Try Again. There was a problem with your request." + ResponseData.message
+                })
+            }
+        }
+        catch (error) {
+            console.error('Error submitting form:', error);
+            toast({
+                variant: "destructive",
+                title: "UnExpected Error!!",
+                description: "Error While Update Seller Account details. Please Try again."
+            })
+        }
+
+    }
+
 
     const handleSubmit = () => { }
 
     useEffect(() => {
 
         fetchSellerDetails()
-        if(sellerDetails !== undefined){
+        if (sellerDetails !== undefined) {
             setCategories(sellerDetails.categories)
             setStoreDescription(sellerDetails.storeDescription)
             setPhoneNo(sellerDetails.phoneNo)
@@ -91,7 +147,7 @@ const ChangeSellerAccountDetails = () => {
             setDistrict(sellerDetails.district)
         }
 
-    }, []);
+    }, [isReload]);
 
     return (
         <>
@@ -99,40 +155,81 @@ const ChangeSellerAccountDetails = () => {
                 <div className="max-w-2xl mx-auto mt-8 p-4">
                     <p className="text-2xl font-bold mb-4">Update Store Details</p>
                     <hr className="py-3" />
-                    <form onSubmit={handleSubmit} className="space-y-4 flex flex-row">
 
-                        {/* <div className="">
-                            <label htmlFor="productImage" className="block mb-1 font-semibold text-lg">Choose image</label>
-                            <input
-                                type="file"
-                                id="file"
-                                name="file"
-                                required
-                                accept="image/*"
-                                className="w-full"
-                            />
+                    <form action={handleDetailsUpdateForm} className="space-y-4 flex flex-col">
+
+                        <div className="flex flex-col space-y-2">
+
+                            <div className="w-full">
+                                <label className="w-1/3 text-right mr-4 text-gray-700">categories:</label>
+                                <input
+                                    type="text"
+                                    name="categories"
+                                    value={categories}
+                                    onChange={(e) => setCategories(e.target.value)}
+                                    className="w-full px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                />
+                            </div>
+
+                            <div className="w-full">
+                                <label className="w-1/3 text-right mr-4 text-gray-700">Store Description:</label>
+                                <textarea
+                                    name="storeDescription"
+                                    value={storeDescription}
+                                    onChange={(e) => setStoreDescription(e.target.value)}
+                                    rows={4}  // You can adjust this number to control initial height
+                                    className="w-full resize-y px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                />
+                            </div>
+
+                            <div className="w-full">
+                                <label className="w-1/3 text-right mr-4 text-gray-700">Phone Number:</label>
+                                <input
+                                    type="text"
+                                    name="phoneNo"
+                                    value={phoneNo}
+                                    onChange={(e) => setPhoneNo(e.target.value)}
+                                    className="w-full px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                />
+                            </div>
+
+                            <div className="w-full">
+                                <label className="w-1/3 text-right mr-4 text-gray-700">Seller Address:</label>
+                                <input
+                                    type="text"
+                                    name="sellerAddress"
+                                    value={sellerAddress}
+                                    onChange={(e) => setSellerAddress(e.target.value)}
+                                    className="w-full px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                />
+                            </div>
+
+                            <div className="w-full">
+                                <label className="w-1/3 text-right mr-4 text-gray-700">District:</label>
+                                <input
+                                    type="text"
+                                    name="district"
+                                    value={district}
+                                    onChange={(e) => setDistrict(e.target.value)}
+                                    className="w-full px-3 py-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                />
+                            </div>
+
                         </div>
 
                         <button
                             type="submit"
                             disabled={loading}
-                            className={` bg-blue-500 text-white py-2 px-5 rounded-3xl ${loading ? 'opacity-50 cursor-not-allowed' : 'hover:bg-blue-600'}`}
+                            className={`w-1/2 bg-blue-500 text-white py-2 px-5 rounded-3xl ${loading ? 'opacity-50 cursor-not-allowed' : 'hover:bg-blue-600'}`}
                         >
-                            {loading ? 'Updating Image...' : 'Update Store Cover Image'}
-                        </button> */}
+                            {loading ? 'Updating Details...' : 'Update Store Details'}
+                        </button>
                     </form>
-
-                    
-                    <p>categories : {categories}</p>
-                    <p>storeDescription : {storeDescription}</p>
-                    <p>phoneNo : {phoneNo}</p>
-                    <p>sellerAddress : {sellerAddress}</p>
-                    <p>district : {district}</p>
 
                 </div>
             </div>
         </>
     )
 }
-export default ChangeSellerAccountDetails
 
+export default ChangeSellerAccountDetails
