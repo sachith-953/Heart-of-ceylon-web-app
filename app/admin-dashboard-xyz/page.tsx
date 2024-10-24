0e11ea4339af954dc9690ae3240b383aebeee767
Rashmika003 <www.rashmikaabeyrathna2019@gmail.com>
2024-10-24 12:33:52 +0530

    admin dashboard basic structure

---
+++ app/admin-dashboard-xyz/page.tsx
@@ -0,0 +1,89 @@
+import ManageAccount from "@/components/dashboard/sellerDashboard/ManageAccount";
+import MyProducts from "@/components/dashboard/sellerDashboard/MyProducts";
+import MySales from "@/components/dashboard/sellerDashboard/MySales";
+import MaxWidthLg from "@/components/MaxWidthLg";
+import Navbar from "@/components/Navbar";
+import { TabsTriggerForSellerDashboard } from "@/components/ui/seller-tabs-trigger";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { BadgeDollarSign, Box, Printer, UserCog, UserPlus, Users } from "lucide-react"
+
+
+
+
+export default function SellerDashbard() {
+    return (
+        <>
+
+            {/* add new navbar designed for the seller*/}
+            <Navbar />
+            
+            <div className="bg-white w-full h-svh pt-1">
+                
+                {/* tabs */}
+                <Tabs defaultValue="Seller Management" className="flex flex-row mx-3 h-full ">
+                    <TabsList className="w-1/4 h-full flex flex-col bg-[#314659] justify-start">
+
+                        {/* <div className="bg-zinc-400 h-20 w-full mb-2">
+                            user account details (if want uncommnet)
+                        </div> */}
+
+                        <TabsTriggerForSellerDashboard className="w-full my-1 flex flex-row" value="Seller Management">
+                            <Box />
+                            <span className="ml-1">Seller Management</span>
+                        </TabsTriggerForSellerDashboard>
+
+                        <TabsTriggerForSellerDashboard className="w-full my-1 flex flex-row" value="All Orders">
+                            <BadgeDollarSign />
+                            <span className="ml-1">All Orders</span>
+                        </TabsTriggerForSellerDashboard>
+
+                        <TabsTriggerForSellerDashboard className="w-full my-1 flex flex-row" value="Product Management">
+                            <UserCog />
+                            <span className="ml-1">Product Management</span>
+                        </TabsTriggerForSellerDashboard>
+
+                        <TabsTriggerForSellerDashboard className="w-full my-1 flex flex-row" value="Logs">
+                            <UserCog />
+                            <span className="ml-1">Logs</span>
+                        </TabsTriggerForSellerDashboard>
+
+                        <TabsTriggerForSellerDashboard className="w-full my-1 flex flex-row" value="Manage Admin Accounts">
+                            <UserCog />
+                            <span className="ml-1">Manage Admin Accounts</span>
+                        </TabsTriggerForSellerDashboard>
+
+                    </TabsList>
+
+                    <div className="w-3/4 max-h-lvh overflow-auto overscroll-auto">
+
+                        <TabsContent value="Seller Management">
+                            
+                        </TabsContent>
+
+                        <TabsContent value="All Orders">
+                            {/* All Orders component here */}
+                        </TabsContent>
+
+                        <TabsContent value="Product Management">
+                            {/*Product Management component here */}
+                        </TabsContent>
+
+                        <TabsContent value="Logs">
+                            {/*Logs component here */}
+                        </TabsContent>
+
+                        <TabsContent value="Manage Admin Accounts">
+                            {/*Manage Admin Accounts component here */}
+                        </TabsContent>
+
+                    </div>
+
+                </Tabs>
+
+            </div>
+
+        </>
+    );
+}
+
+
